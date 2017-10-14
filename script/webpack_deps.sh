bf3e1ecc615c4269057085a915499b89d43facbd
Bob Strecansky <bob.strecansky@gmail.com>
2017-10-14 09:42:41 -0400

    Initial Commit

---
+++ script/webpack_deps.sh
@@ -0,0 +1,7 @@
+npm install --save-dev webpack
+npm install --save-dev webpack-dev-server
+npm install --save-dev html-webpack-plugin
+npm install --save-dev babel-core babel-loader babel-preset-env babel-preset-react
+npm install --save react react-dom
+npm install --save-dev css-loader extract-text-webpack-plugin
+npm install --save-dev file-loader
